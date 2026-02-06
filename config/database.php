ee45afda1f67cad08331de386e427b7d41bcd9c8
jh0207 <143569062+jh0207@users.noreply.github.com>
2026-02-07 01:51:04 +0800

    Add env-based configuration and ignore local files

--- config/database.php
+++ config/database.php
@@ -1,19 +1,21 @@
 <?php
 
+$dbDebug = getenv('DB_DEBUG');
+
 return [
     'default'         => 'mysql',
     'time_query_rule' => [],
     'connections'     => [
         'mysql' => [
             'type'            => 'mysql',
-            'hostname'        => '127.0.0.1',
-            'database'        => 'tg_cert_bot',
-            'username'        => 'root',
-            'password'        => '',
-            'hostport'        => '3306',
+            'hostname'        => getenv('DB_HOST') ?: '127.0.0.1',
+            'database'        => getenv('DB_NAME') ?: 'tg_cert_bot',
+            'username'        => getenv('DB_USER') ?: 'root',
+            'password'        => getenv('DB_PASS') ?: '',
+            'hostport'        => getenv('DB_PORT') ?: '3306',
             'charset'         => 'utf8mb4',
             'prefix'          => '',
-            'debug'           => true,
+            'debug'           => $dbDebug === false ? true : filter_var($dbDebug, FILTER_VALIDATE_BOOLEAN),
             'fields_strict'   => true,
             'resultset_type'  => 'array',
             'auto_timestamp'  => true,
