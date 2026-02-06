5e598e486ce70223bf679ab34ae54d7950c2bd89
jh0207 <143569062+jh0207@users.noreply.github.com>
2026-02-07 02:37:06 +0800

    Read .env values in config

--- config/database.php
+++ config/database.php
@@ -1,19 +1,23 @@
 <?php
 
+require_once __DIR__ . '/env.php';
+
+$dbDebug = env_value('DB_DEBUG');
+
 return [
     'default'         => 'mysql',
     'time_query_rule' => [],
     'connections'     => [
         'mysql' => [
             'type'            => 'mysql',
-            'hostname'        => '127.0.0.1',
-            'database'        => 'tg_cert_bot',
-            'username'        => 'root',
-            'password'        => '',
-            'hostport'        => '3306',
+            'hostname'        => env_value('DB_HOST', '127.0.0.1'),
+            'database'        => env_value('DB_NAME', 'tg_cert_bot'),
+            'username'        => env_value('DB_USER', 'root'),
+            'password'        => env_value('DB_PASS', ''),
+            'hostport'        => env_value('DB_PORT', '3306'),
             'charset'         => 'utf8mb4',
             'prefix'          => '',
-            'debug'           => true,
+            'debug'           => $dbDebug === null ? true : filter_var($dbDebug, FILTER_VALIDATE_BOOLEAN),
             'fields_strict'   => true,
             'resultset_type'  => 'array',
             'auto_timestamp'  => true,
--- config/tg.php
+++ config/tg.php
@@ -1,10 +1,14 @@
 <?php
 
+require_once __DIR__ . '/env.php';
+
+$ownerLock = env_value('OWNER_LOCK');
+
 return [
-    'token' => getenv('TG_BOT_TOKEN') ?: 'change-me',
-    'api_base' => 'https://api.telegram.org',
-    'owner_lock' => true,
-    'acme_path' => '/root/.acme.sh/acme.sh',
-    'acme_server' => 'letsencrypt',
-    'cert_export_path' => '/www/wwwroot/cert.com/ssl/',
+    'token' => env_value('TG_BOT_TOKEN', ''),
+    'api_base' => env_value('TG_API_BASE', 'https://api.telegram.org'),
+    'owner_lock' => $ownerLock === null ? true : filter_var($ownerLock, FILTER_VALIDATE_BOOLEAN),
+    'acme_path' => env_value('ACME_PATH', '/root/.acme.sh/acme.sh'),
+    'acme_server' => env_value('ACME_SERVER', 'letsencrypt'),
+    'cert_export_path' => env_value('CERT_EXPORT_PATH', '/www/wwwroot/cert.com/ssl/'),
 ];
